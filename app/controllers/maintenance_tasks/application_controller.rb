989e000afab0e4c2c959cc250035d935552e5ace
Ahmed R. Hussein <elrefat@gmail.com>
2026-05-26 20:31:31 -0400

    Add a ? navbar button as a visible shortcut hint
    The keyboard-shortcuts dialog was discoverable only by typing the
    literal key. Adding a kbd-styled "?" button in the navbar makes the
    shortcut self-documenting: hover surfaces the tooltip "Keyboard
    shortcuts (press ?)", click opens the dialog, and the button itself
    visually mimics a keyboard key so users learn the shortcut from
    seeing it.
    
    Click handler is added to the existing IIFE delegation. CSP hashes
    for both inline blocks recomputed.

--- app/controllers/maintenance_tasks/application_controller.rb
+++ app/controllers/maintenance_tasks/application_controller.rb
@@ -11,7 +11,7 @@ module MaintenanceTasks
       policy.style_src_elem(
         BULMA_CDN,
         # <style> tag in app/views/layouts/maintenance_tasks/application.html.erb
-        "'sha256-ox7mE/EKNu1LSlPjykR0D9f1I4lweFCFspT0GIvXq2M='",
+        "'sha256-dHuqkGFbDMNYEwxVRxIjGA21cwLvxF7bD/tjtQiZVvQ='",
       )
       capybara_lockstep_scripts = [
         "'sha256-1AoN3ZtJC5OvqkMgrYvhZjp4kI8QjJjO7TAyKYiDw+U='",
@@ -19,7 +19,7 @@ module MaintenanceTasks
       ] if defined?(Capybara::Lockstep)
       policy.script_src_elem(
         # <script> tag in app/views/layouts/maintenance_tasks/application.html.erb
-        "'sha256-9mN4sU51jxsj135wbqKpRgEnliX6Sc1uTmzkFJuQaAI='",
+        "'sha256-nxLMmyBS4AWECZCMCdJYoFL64XY+62D6YAoa99AeKbo='",
         # <script> tag for capybara-lockstep
         *capybara_lockstep_scripts,
       )
